0352345b7d4206839551aa7cf0ff01f857c51a73
kapija <140849011+kapijawastaken@users.noreply.github.com>
2024-08-27 12:16:08 +0200

    Add files via upload

--- home/ri/.config/fish/config.fish
+++ home/ri/.config/fish/config.fish
@@ -6,4 +6,16 @@ fzf --fish | source
 alias m="sudo micro"
 alias fishconf="sudo micro ~/.config/fish/config.fish"
 fish_add_path -P ~/.cargo/bin
+set PF_INFO "ascii title os cpu de pkgs uptime"
+export PF_INFO
+set PF_COL1 1
+export PF_COL1
+set PF_COL3 COL1
+export PF_COL3
+set fish_color_cwd red
+set fish_color_user red
+function fish_greeting
+    set_color red
+    echo "Welcome, Ri!"
+end
 pfetch
