9396a8b354b05671873f35fa8ef6b2c2e5167d22
kapija <140849011+kapijawastaken@users.noreply.github.com>
2024-08-27 09:21:04 +0200

    Add files via upload

--- home/ri/.config/fish/config.fish
+++ home/ri/.config/fish/config.fish
@@ -1,1 +1,9 @@
-(placeholder)
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+# Set up fzf key bindings
+fzf --fish | source
+alias m="sudo micro"
+alias fishconf="sudo micro ~/.config/fish/config.fish"
+fish_add_path -P ~/.cargo/bin
+pfetch
