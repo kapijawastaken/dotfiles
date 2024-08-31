dd93404f92d949e1f228f5279b0e8eb0513ef59a
kapija <140849011+kapijawastaken@users.noreply.github.com>
2024-08-31 16:21:55 +0200

    Add files via upload

--- home/ri/.config/fish/config.fish
+++ home/ri/.config/fish/config.fish
@@ -3,19 +3,19 @@ if status is-interactive
 end
 # Set up fzf key bindings
 fzf --fish | source
-alias m="sudo micro"
-alias fishconf="sudo micro ~/.config/fish/config.fish"
+alias m="micro"
+alias fishconf="micro ~/.config/fish/config.fish"
 fish_add_path -P ~/.cargo/bin
-set PF_INFO "ascii title os cpu de pkgs uptime"
+set PF_INFO "ascii title os pkgs uptime"
 export PF_INFO
 set PF_COL1 1
 export PF_COL1
 set PF_COL3 COL1
 export PF_COL3
-set fish_color_cwd red
-set fish_color_user red
+set fish_color_cwd brred
+set fish_color_user brred
 function fish_greeting
-    set_color red
+    set_color brred
     echo "Welcome!"
 end
 pfetch
