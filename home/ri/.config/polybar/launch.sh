eb423f4899254571f37e908812d7467eb0ef3521
kapija <140849011+kapijawastaken@users.noreply.github.com>
2025-09-04 17:24:36 +0200

    Add files via upload

--- home/ri/.config/polybar/launch.sh
+++ home/ri/.config/polybar/launch.sh
@@ -6,3 +6,12 @@ killall -q polybar
 # Launch Polybar
 polybar top -c ~/.config/polybar/config.ini &
 
+sleep 0.5
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Launch Polybar
+polybar top -c ~/.config/polybar/config.ini &
+
+
